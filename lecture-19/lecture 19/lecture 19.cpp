8257847470d07ac90329b4df468a7bb634a039a3
Yan <yan.matveyev@gmail.com>
2024-01-26 19:00:30 +0200

    Moved first year university repos to a single folder

---
+++ lecture-19/lecture 19/lecture 19.cpp
@@ -0,0 +1,123 @@
+﻿
+
+#include <iostream>
+using namespace std;
+struct T_List
+{
+    T_List* next;
+    //std::string surname;
+    int age;
+};
+void ADD(T_List* head, int age)
+{
+    T_List* p = new T_List;
+    p->age = age;
+    p->next = head->next;
+    head->next = p;
+}
+void PRINT(T_List* head)
+{
+    T_List* p = head->next;
+    while (p != nullptr)
+    {
+        std::cout << p->age << std::endl;
+        p = p->next;
+    }
+}
+void DELETE(T_List* head)
+{
+    T_List* tmp;
+    T_List* p = head;
+    while (p->next != nullptr)
+    {
+        if (p->next->age % 2 == 0)
+        {
+            tmp = p->next;
+            p->next = p->next->next;            
+            delete tmp;
+        }
+        else 
+            p = p->next;
+        
+    }
+}
+void CLEAR(T_List* head)
+{
+    T_List* tmp;
+    T_List* p = head->next;
+    while (p != nullptr)
+    {
+        tmp = p;
+        p = p->next;
+        delete tmp;
+    }
+}
+void DUPLICATE(T_List* head)
+{
+    T_List* p = head->next;
+    while (p != nullptr)
+    {
+        if (p->age % 2 == 1)
+        {
+            T_List* q = new T_List;
+            q->next = p->next;
+            p->next = q;
+            q->age = p->age;
+            p = p->next; 
+        }
+        p = p->next;
+    }
+}
+void SWAPSORT(T_List* head)
+{
+    /*for ( int i=0; i<n-1; i++ )
+        for (int j = 0; j < n+1; j++)*/
+    T_List* p = head->next;
+    while (p->next != nullptr)
+    {
+        T_List* q = p->next;
+        while (q->next != nullptr)
+        {
+            if (p->age > q->age)
+                swap(p->age, q->age);
+            q = q->next;
+        }
+        p = p->next;
+    }
+}
+int main()
+{
+    T_List* head = new T_List;
+    head->next = nullptr;
+
+    // создание списка 
+    ADD(head, 10);
+    ADD(head, 2);
+    ADD(head, 3);
+    ADD(head, 5);
+    ADD(head, 6);
+
+    PRINT(head);
+
+    cout << "====" << endl;
+
+    SWAPSORT(head);
+    PRINT(head);
+
+    cout << "====" << endl;
+
+    DELETE(head);
+    PRINT(head);
+
+    cout << "====" << endl;
+
+    DUPLICATE(head);
+    PRINT(head);
+
+    CLEAR(head);
+    delete head;
+    return 0;
+}
+// удаление занимает тета(1)
+// а в массиве бы это заняло O(n)
+
---
+++ lecture22_Matrix/lecture22/lecture22.cpp
@@ -0,0 +1,125 @@
+﻿
+
+#include <iostream>
+using namespace std;
+class Matrix
+{
+public:
+    // Конструктор матрицы
+    Matrix(int n,int m)
+    {
+        std::cout << "Constructor" << endl;
+        m_n = n;
+        m_m = m;
+        m_mat = new int* [m_n];
+        for (int i = 0; i < m_n; i++)
+            m_mat[i] = new int[m_m];
+    }
+    //Правило трёх C++
+    // Если класс или структура определяет один из следующих методов,
+    // Деструктор
+    //Конструктор копирования
+    //Оператор присваивания копированием
+    // то они должны явным образом определить все три метода
+
+    //Конструктор копирования
+    Matrix(const Matrix& mat)
+    {
+        m_n = mat.m_n;
+        m_m = mat.m_m;
+
+        m_mat = new int* [m_n];
+        for (int i = 0; i < m_n; i++)
+            m_mat[i] = new int[m_m];
+        for (int i = 0; i < m_n; i++)
+            for (int j = 0; j < m_m; j++)
+                m_mat[i][j] = mat.m_mat[i][j];        
+    }
+    
+    //Оператор присваивания копированием
+    Matrix& operator=(const Matrix& mat)
+    {
+        cout << "Operator =" << endl;
+        m_n = mat.m_n;
+        m_m = mat.m_m;
+
+        for (int i = 0; i < m_n; i++)
+            for (int j = 0; j < m_m; j++)
+                m_mat[i][j] = mat.m_mat[i][j];
+        return *this;
+        //*this содержит указатель на текущий объект класса
+    }
+    //Оператор сложения
+    Matrix operator+(const Matrix& mat)
+    {
+        cout << "Operator +" << endl;
+        Matrix tmp(2, 3);
+        for (int i = 0; i < m_n; i++)
+            for (int j = 0; j < m_m; j++)
+                tmp.m_mat[i][j] = m_mat[i][j] + mat.m_mat[i][j];
+        return tmp;
+    }
+    //Оператор умножения
+    Matrix operatormultiplier(const Matrix& mat)
+    {
+        cout << "Operator *" << endl;
+        Matrix tmp(2, 3);
+        for (int i = 0; i < m_n; i++)
+            for (int j = 0; j < m_m; j++)
+                tmp.m_mat[i][j] = m_mat[i][j] * mat.m_mat[i][j];
+        return tmp;
+    }
+    //Деструктор нужен чтобы удалять динмаические массивы
+    //Чтобы не было утечки памяти
+    ~Matrix()//пишем через знак ~(тильда) он использует для деструкторов в вижаке
+    {
+        cout << "Destuctor" << endl;
+        for (int i = 0; i < m_n; i++)
+            delete[] m_mat[i];
+        delete m_mat;
+    }
+    // с помощью friend можно обращаться к private
+    friend istream& operator>>(istream& in, Matrix& mat);
+    friend ostream& operator<<(ostream& out, Matrix& mat);
+private:
+    int m_n,m_m;// поле класса
+    int** m_mat;// динамический массив потому что мы не знаем каких размеров будет матрица
+};
+// перегрузка оператора ввоода
+// перегрузыка - это расширение оператора добавление новой функции
+istream& operator>>(istream& in, Matrix& mat)
+//& ссылка на istream и принимает на вход ссыллку на матрицу
+{
+    for (int i=0; i<mat.m_n;i++)
+        for(int j=0;j<mat.m_m;j++)
+            in >> mat.m_mat[i][j];    
+    return in;
+}
+// перегрузыка оператора вывода
+ostream& operator<<(ostream& out, Matrix& mat)
+{
+    out << "Matrix" << mat.m_n << "x" << mat.m_m << endl;
+    for (int i = 0; i < mat.m_n; i++)
+    {
+        for (int j = 0; j < mat.m_m; j++)
+            out << mat.m_mat[i][j] << " ";
+        out << endl;
+    }
+    return out;
+}
+
+int main()
+{
+    Matrix A(2, 3);
+    cin >> A;
+    cout << A << endl;
+
+    Matrix B(2, 3);
+    cin >> B;
+    cout << B << endl;
+
+    Matrix C(2, 3);
+    C = A + B;
+    cout << C << endl;
+}
+
